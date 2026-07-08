054644a3919ade03d5051a56877cf041085bb381
Zhen Nan <lowzhennan@gmail.com>
2026-07-08 19:29:17 +0800

    create a btn call change language
    What I have done is create a new button and function to change language and use boolean to check for both language when comparing who is the winner.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+isEnglish = true;
+
 function rollDice1() {
   const randomNumber1 = Math.floor(Math.random() * 6) + 1;
   // debugging use
-  // console.log("Get Dice Number 1:", randomNumber1);
+  console.log("Get Dice Number 1:", randomNumber1);
 
   switch (randomNumber1) {
     case 1:
@@ -39,7 +41,7 @@ function rollDice1() {
 function rollDice2() {
   const randomNumber2 = Math.floor(Math.random() * 6) + 1;
   // debugging use
-  // console.log("Get Dice Number 2:", randomNumber2);
+  console.log("Get Dice Number 2:", randomNumber2);
 
   switch (randomNumber2) {
     case 1:
@@ -78,11 +80,42 @@ function playTheGame() {
   const dice1 = rollDice1();
   const dice2 = rollDice2();
 
+  const h1 = document.querySelector("h1");
+  const winner1_Text = isEnglish ? "Player 1 Wins!" : "恭喜你赢了!";
+  const winner2_Text = isEnglish
+    ? "Player 2 Wins!"
+    : "哎哟哟! 你输了! 继续加油!";
+
+  const draw_Text = isEnglish ? "Draw!" : "打成平手! 再来一次!";
+
   if (dice1 > dice2) {
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    document.querySelector("h1").innerHTML = winner1_Text;
   } else if (dice1 < dice2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    document.querySelector("h1").innerHTML = winner2_Text;
   } else {
-    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = draw_Text;
+  }
+}
+
+function changeLanguage() {
+  isEnglish = !isEnglish;
+  const h1 = document.querySelector("h1");
+  const p1 = document.querySelector(".img1").previousElementSibling;
+  const p2 = document.querySelector(".img2").previousElementSibling;
+  const btn_RollDice = document.querySelector(".btnRollDice");
+  const btn_ChangeLanguage = document.querySelector(".btnChangeLanguage");
+
+  if (isEnglish) {
+    h1.innerHTML = "Roll the Dice!";
+    p1.innerHTML = "Player 1";
+    p2.innerHTML = "Player 2";
+    btn_RollDice.innerHTML = "Roll Dice";
+    btn_ChangeLanguage.innerHTML = "Change Language";
+  } else {
+    h1.innerHTML = "转骰子游戏";
+    p1.innerHTML = "玩家 1";
+    p2.innerHTML = "玩家 2";
+    btn_RollDice.innerHTML = "转骰子";
+    btn_ChangeLanguage.innerHTML = "切换语言";
   }
 }
